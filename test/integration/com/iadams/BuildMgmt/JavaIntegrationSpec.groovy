11dd74823a5108f09c4f3b260d2527929e6aed0a
Iain Adams <iwarapter@hotmail.com>
2014-02-23 21:24:54 +0000

    Added the new test for the (to-do) java domain class, this class will have an instance of JavaRuntime and JavaVirtualMachine.

---
+++ test/integration/com/iadams/BuildMgmt/JavaIntegrationSpec.groovy
@@ -0,0 +1,33 @@
+package com.iadams.BuildMgmt
+
+import com.iadams.BuildMgmt.Java
+import com.iadams.BuildMgmt.JavaRuntime
+import com.iadams.BuildMgmt.JavaVirtualMachine
+
+import spock.lang.*
+import grails.plugin.spock.*
+
+class JavaIntegrationSpec extends IntegrationSpec {
+	
+	def runtime
+	def jvm
+
+	def "Saving our first JavaRuntime to the database"() {
+		
+		given: "A brand new java install"
+		def J = new Java(name: "JDK 1.6", ver: "1.6", vendor: "Oracle", home: "C:\\JDK", jvm: jvm.id, runtime: runtime.id)
+		
+		when: "we save the java install"
+		J.save()
+		
+		then: "we find the java install in the db"
+		J.errors.errorCount == 0
+		J.id != null
+		Java.get(J.id).name == J.name
+	}
+	
+	def setup() {
+		runtime = new JavaRuntime(name: "Java(TM) SE Runtime Environment", ver: "1.6").save(failOnError: true)
+		jvm = new JavaVirtualMachine(name: "JVM name", ver: "1.6", vendor: "Oracle").save(failOnError: true)
+	}
+}

9c1d1b53c8b97e0fda1b2ba0d489869f3c543759
Iain Adams <iwarapter@hotmail.com>
2014-02-23 19:14:15 +0000

    Added the initial integration test for the machine domain-class.

---
+++ test/integration/com/iadams/BuildMgmt/MachineIntegrationSpec.groovy
@@ -0,0 +1,23 @@
+package com.iadams.BuildMgmt
+
+import com.iadams.BuildMgmt.Machine
+
+import spock.lang.*
+import grails.plugin.spock.*
+
+class MachineIntegrationSpec extends IntegrationSpec {
+
+	def "Saving our first user to the database"() {
+		
+		given: "A brand new machine"
+		def laptop = new Machine(name: "Laptop", os: "Windows", os_ver: "6.1", os_arch: "x64")
+		
+		when: "we save the user"
+		laptop.save()
+		
+		then: "we find the user in the db"
+		laptop.errors.errorCount == 0
+		laptop.id != null
+		User.get(laptop.id).name == laptop.name
+	}
+}

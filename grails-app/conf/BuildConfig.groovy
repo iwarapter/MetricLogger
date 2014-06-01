dac5ab6d0aaf0791033ce1cdf8ba0d1dd32d312f
Iain Adams <iwarapter@hotmail.com>
2014-06-01 18:26:27 +0100

    Altered the domain relationships for Plugin/Project, this is now Many-To-Many with the Plugin being the owner. Removed a domain uml modeling plugin also.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -66,6 +66,5 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
-		compile ":class-domain-uml:0.1.5"
     }
 }
--- grails-app/domain/com/iadams/BuildMgmt/Plugin.groovy
+++ grails-app/domain/com/iadams/BuildMgmt/Plugin.groovy
@@ -3,7 +3,11 @@ package com.iadams.BuildMgmt
 class Plugin {
 	
 	String name
+	
+	static hasMany = [ projects : Project ]
+	static belongsTo = Project
 
     static constraints = {
+		name unique: true
     }
 }

4554febb74a96e82c833d19fb9b3d42d102bda3d
Iain Adams <iwarapter@hotmail.com>
2014-06-01 18:22:51 +0100

    Added the logic for the TaskRestController.groovy

--- grails-app/controllers/com/iadams/BuildMgmt/TaskRestController.groovy
+++ grails-app/controllers/com/iadams/BuildMgmt/TaskRestController.groovy
@@ -1,13 +1,46 @@
 package com.iadams.BuildMgmt
 
+import grails.converters.JSON
+import grails.converters.XML
+
 class TaskRestController {
 	
 	def list() {
+		
+		def body
+		body = Task.list()
+		
+		withFormat {
+			json { render body as JSON }
+			xml { render body as XML }
+		}
 	}
 	
 	def show(String name) {
+		def body
+		body = Task.findByName(name)
+		
+		withFormat {
+			json { render body as JSON }
+			xml { render body as XML }
+		}
 	}
 	
 	def save() {
+		def body
+		def task = new Task(params.task)
+		if(task.validate() && task.save()) {
+			response.status = 201
+			body = [id: task.id]
+		}
+		else {
+			response.status = 403
+			body = [error: "Invalid data"]
+		}
+
+		withFormat {
+			json { render body as JSON }
+			xml { render body as XML }
+		}
 	}
 }
